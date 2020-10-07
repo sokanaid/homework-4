f7da80c1839a45220a40e4a73562ff1ec18e9f12
sokanaid <71149930+sokanaid@users.noreply.github.com>
2020-10-07 23:29:11 +0300

    Add files via upload

---
+++ A/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace A
+{
+
+    class Program
+    {
+
+        static Random rnd = new Random();
+        public static int[] CreateArray(int n)
+        {
+            int[] array = new int[n];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next(-1, 2);
+            }
+            return array;
+        }
+        public static void ShowArray(ref int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                
+                if (i % 10 == 0 && i!=0)
+                {
+                    Console.WriteLine();
+                }
+                Console.Write("{0} ", array[i]);
+            }
+            Console.WriteLine();
+        }
+        public static int[] MergeArray(ref int[] A, ref int[] B)
+        {
+            int[] C = new int[Math.Max(A.Length, B.Length)];
+            for(int i=0; i < C.Length;i++)
+            {
+                if(i%2==0 && i < A.Length)
+                {
+                    C[i] = A[i];
+                }
+                if(i%2==1 && i < B.Length)
+                {
+                    C[i] = B[i];
+                }
+            }
+            return C;
+        }
+        static void Main(string[] args)
+        {
+
+            int[] A = CreateArray(int.Parse(Console.ReadLine()));
+            int[] B = CreateArray(int.Parse(Console.ReadLine()));
+            int [] C =MergeArray(ref A, ref B);
+
+            Console.WriteLine("Массив A");
+            ShowArray(ref A);
+            Console.WriteLine("Массив B");
+            ShowArray(ref B);
+            Console.WriteLine("Массив C");
+            ShowArray(ref C);
+
+        }
+    }
+}
---
+++ C/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Dynamic;
+
+namespace C
+{
+    class Program
+    {
+        /// <summary>
+        /// Массив степеней двоек
+        /// </summary>
+        /// <param name="n">колличество элеменотов</param>
+        /// <returns>Массив степеней двоек</returns>
+        public static int[] CreateArray2(int n)
+        {
+            int[] array=new int [n];
+            for(int i=1,j=0 ;j<n; i *= 2, j++)
+            {
+                array[j] = i;
+            }
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("{0} ", array[i]);
+            }
+            Console.WriteLine();
+            return array;
+        }
+        /// <summary>
+        /// Массив нечетных элементов
+        /// </summary>
+        /// <param name="n">колличество элементов массива</param>
+        /// <returns>Массив нечетных элементов</returns>
+        public static int[] CreateArray1(int n)
+        {
+            int[] array = new int[n];
+            for (int i = 1, j = 0; j < n; i+= 2, j++)
+            {
+                array[j] = i;
+            }
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("{0} ", array[i]);
+            }
+            Console.WriteLine();
+            return array;
+        }
+        /// <summary>
+        /// массив вида a+k*d
+        /// </summary>
+        /// <param name="n">колличество элементов массива</param>
+        /// <param name="a">число</param>
+        /// <param name="d">число </param>
+        /// <returns>массив</returns>
+        public static int[] CreateArray3(int n,int a,int d)
+        {
+            int[] array = new int[n];
+            for (int i = a, j = 0; j < n; i += d, j++)
+            {
+                array[j] = i;
+            }
+            for (int i =0; i < n; i++)
+            {
+                Console.Write("{0} ", array[i]);
+            }
+            Console.WriteLine();
+            return array;
+
+        }
+        /// <summary>
+        /// Последовательность Фибоначи
+        /// </summary>
+        /// <param name="n">колличество элементов</param>
+        /// <returns>массив</returns>
+        public static int[] CreateArray4(int n)
+        {
+            int[] array = new int[n];
+            array[0] = array[1] = 1;
+            for (int j = 2; j < n; j++)
+            {
+                array[j] = array[j-1]+array[j-2];
+            }
+            for(int i=n-1; i>=0; i--)
+            {
+                Console.Write("{0} ",array[i]);
+            }
+            Console.WriteLine();
+            return array;
+        }
+        static void Main(string[] args)
+        {
+            CreateArray2(6);
+            CreateArray1(6);
+            CreateArray3(6,2,1);
+            CreateArray4(6);
+        }
+    }
+}
